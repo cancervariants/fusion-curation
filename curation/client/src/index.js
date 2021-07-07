3a27b1b894bf4e7345a6f39e355f0e7ce7a832c9
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-07-07 15:46:15 -0400

    Pre-meeting progress: ported most basic functions from original wireframe

--- curation/client/src/index.js
+++ curation/client/src/index.js
@@ -1,6 +1,12 @@
-import { React, Fragment, Component, useState } from 'react';
+import {
+  React, Fragment, useState,
+} from 'react';
 import ReactDOM from 'react-dom';
-import { FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, TextField } from '@material-ui/core';
+import {
+  Box, Button, Container, FormControl, FormLabel,
+  FormControlLabel, Radio, RadioGroup, TextField, AppBar, Typography, Toolbar,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 // TODO return to https://reactjs.org/docs/forms.html
 // basically riff on this https://redux-form.com/8.2.2/examples/material-ui/
@@ -18,7 +24,7 @@ function RadioOption({ option, stateValue, stateFunction }) {
 
 function FormRadio({ name, prompt, state }) {
   return (
-    <>
+    <Box p={1}>
       <FormControl component="fieldset">
         <FormLabel component="legend">{prompt}</FormLabel>
         <RadioGroup aria-label={name} name={name}>
@@ -35,64 +41,204 @@ function FormRadio({ name, prompt, state }) {
         </RadioGroup>
       </FormControl>
       <p />
-    </>
+    </Box>
   );
 }
 
-function FormJunctions() {
+function FormJunctions({
+  junction5Prime, setJunction5Prime, junction3Prime, setJunction3Prime,
+}) {
+  // TODO this is just a mess
+  function handle5PrimeChange(label, event) {
+    if (label === "5' End Transcript") {
+      setJunction5Prime({
+        ...junction5Prime,
+        transcript: event.target.value,
+      });
+    } else if (label === "5' End Exon Number") {
+      setJunction5Prime({
+        ...junction5Prime,
+        exon_number: event.target.value,
+      });
+    } else if (label === "5' End Gene") {
+      setJunction5Prime({
+        ...junction5Prime,
+        gene: event.target.value,
+      });
+    } else if (label === "5' End Chromosome") {
+      setJunction5Prime({
+        ...junction5Prime,
+        chr: event.target.value,
+      });
+    } else if (label === "5' End Position") {
+      setJunction5Prime({
+        ...junction5Prime,
+        pos: event.target.value,
+      });
+    }
+  }
+
+  function handle3PrimeChange(label, event) {
+    if (label === "3' End Transcript") {
+      setJunction3Prime({
+        ...junction3Prime,
+        transcript: event.target.value,
+      });
+    } else if (label === "3' End Exon Number") {
+      setJunction3Prime({
+        ...junction3Prime,
+        exon_number: event.target.value,
+      });
+    } else if (label === "3' End Gene") {
+      setJunction3Prime({
+        ...junction3Prime,
+        gene: event.target.value,
+      });
+    } else if (label === "3' End Chromosome") {
+      setJunction3Prime({
+        ...junction3Prime,
+        chr: event.target.value,
+      });
+    } else if (label === "3' End Position") {
+      setJunction3Prime({
+        ...junction3Prime,
+        pos: event.target.value,
+      });
+    }
+  }
+
   return (
-    <>
+    <Box p={1}>
       <FormLabel component="legend">Record chimeric transcript junctions and associated genes:</FormLabel>
       <form noValidate autoComplete="off">
         {
           [
-            "5' End Transcript", "5' End Exon Number", "5' End Gene", "5' End Chromosome", "5' End Position",
-            "3' End Transcript", "3' End Exon Number", "3' End Gene", "3' End Chromosome", "3' End Position",
-          ].map((item) => (
-            <TextField key={item} label={item} />
+            [["5' End Transcript", "5' End Exon Number", "5' End Gene", "5' End Chromosome", "5' End Position"], handle5PrimeChange],
+            [["3' End Transcript", "3' End Exon Number", "3' End Gene", "3' End Chromosome", "3' End Position"], handle3PrimeChange],
+          ].map((row) => (
+            <Box p={1} key={row[0][0]}>
+              {
+                row[0].map((item) => (
+                  <TextField key={item} label={item} onChange={(event) => row[1](item, event)} />
+                ))
+              }
+            </Box>
           ))
         }
       </form>
+    </Box>
+  );
+}
+
+function FormFunctionalDomains({ setRetainedDomains, setRetainedDomainGenes }) {
+  function domainHandleChange(event) {
+    setRetainedDomains(event.target.value);
+  }
+
+  function geneHandleChange(event) {
+    setRetainedDomainGenes(event.target.value);
+  }
+
+  return (
+    <>
+      <Box p={1}>
+        <FormLabel component="legend">Record predicted meaningful protein functional domains preserved:</FormLabel>
+        <form noValidate autoComplete="off" onChange={domainHandleChange}>
+          <TextField />
+        </form>
+      </Box>
+      <Box p={1}>
+        <FormLabel component="legend">Record associated genes:</FormLabel>
+        <form noValidate autoComplete="off" onChange={geneHandleChange}>
+          <TextField />
+        </form>
+      </Box>
     </>
   );
 }
 
-function FormFunctionalDomains() {
+function CausEventInfo() {
   return (
-    <div id="record-functional-domains">
-      <FormLabel component="legend">Record predicted meaningful protein functional domains preserved:</FormLabel>
-      <form noValidate autoComplete="off">
-        <TextField />
-      </form>
-      <FormLabel component="legend">Record associated genes:</FormLabel>
-      <form noValidate autoComplete="off">
-        <TextField />
-      </form>
-    </div>
+    <>
+      <Box p={1}>
+        <form noValidate autoComplete="off">
+          <FormControl component="fieldset">
+            <FormLabel component="legend">
+              Record event type:
+            </FormLabel>
+            <RadioGroup aria-label="event-type" name="event-type">
+              <FormControlLabel value="rearrangement" control={<Radio />} label="Rearrangement" />
+              <FormControlLabel value="read-through" control={<Radio />} label="Read-through" />
+              <FormControlLabel value="trans-splicing" control={<Radio />} label="Trans-splicing" />
+            </RadioGroup>
+          </FormControl>
+        </form>
+      </Box>
+    </>
   );
 }
 
-function CausEventInfo() { // TODO keep working here
+function Submit({ handler }) {
+  return (
+    <Box p={1}>
+      <Button variant="contained" color="primary" onClick={handler}>Submit</Button>
+    </Box>
+  );
+}
+
+function Response({ jsonValue, readableValue }) {
   return (
     <>
-      <form noValidate autoComplete="off">
-        <FormLabel component="legend">Record predicted meaningful protein functional domains preserved and associated genes:</FormLabel>
-        <TextField />
-      </form>
+      <Box p={1}>
+        <TextField
+          id="response-json"
+          label="JSON"
+          multiline
+          variant="outlined"
+          InputProps={{
+            readOnly: true,
+          }}
+          value={jsonValue}
+          style={{ width: 700 }}
+        />
+      </Box>
+      <Box p={1}>
+        <TextField
+          id="response-hgvs"
+          label="HGVS-like"
+          multiline
+          variant="outlined"
+          InputProps={{
+            readOnly: true,
+          }}
+          value={readableValue}
+          style={{ width: 700 }}
+        />
+      </Box>
     </>
   );
 }
 
 function FormParent() {
+  // visibility handlers
   const [showRfPreserved, setShowRfPreserved] = useState(false);
   const [showFuncDomains, setShowFuncDomains] = useState(false);
   const [showJunctions, setShowJunctions] = useState(false);
   const [showCausEvent, setShowCausEvent] = useState(false);
   const [showCausEventInfo, setShowCausEventInfo] = useState(false);
+  const [showSubmit, setShowSubmit] = useState(false);
+  const [showResponse, setShowResponse] = useState(false);
 
+  // form value handlers
   const [proteinCodingValue, setProteinCodingValue] = useState(null);
   const [rfPreserved, setRfPreserved] = useState(null);
+  const [retainedDomains, setRetainedDomains] = useState(''); // TODO switch to array
+  const [retainedDomainsGenes, setRetainedDomainGenes] = useState(''); // TODO switch to array
+  const [junction5Prime, setJunction5Prime] = useState({});
+  const [junction3Prime, setJunction3Prime] = useState({});
   const [causEvent, setCausEvent] = useState(null);
+  const [responseJSON, setResponseJSON] = useState('');
+  const [responseHuman, setResponseReadable] = useState('');
 
   const handleSetProteinCoding = (oldValue, newValue) => {
     if (oldValue !== newValue) {
@@ -125,13 +271,60 @@ function FormParent() {
 
   const handleSetCausEvent = (oldValue, newValue) => {
     if (oldValue !== newValue) {
-      setShowCausEvent(newValue);
+      setCausEvent(newValue);
       if (newValue === 'Yes') {
         setShowCausEventInfo(true);
+        setShowSubmit(true);
+      } else if (newValue === 'No') {
+        setShowCausEventInfo(false);
+        setShowSubmit(true);
       } else {
         setShowCausEventInfo(false);
+        setShowSubmit(false);
       }
     }
+  };
+
+  const handleSubmit = () => {
+    // setResponseJSON('asldkjsdaf');
+    // setResponseReadable('werowerouxc');
+    const jsonOutput = {};
+    if (proteinCodingValue === 'Yes') {
+      if (rfPreserved === 'Yes') {
+        jsonOutput.r_frame_preserved = true;
+        if (retainedDomains !== '') {
+          const domain = {
+            domain_name: retainedDomains,
+          };
+          if (retainedDomainsGenes !== '') {
+            domain.gene = retainedDomainsGenes;
+          }
+          jsonOutput.retainedDomains = [domain];
+        }
+      } else if (rfPreserved === 'No') {
+        jsonOutput.r_frame_preserved = false;
+      }
+    }
+
+    // TODO junctions:
+    // * gene sub-object (get id)
+    // * genomic_coordinate sub-object (chr, position)
+    if (Object.keys(junction5Prime).length > 0) {
+      jsonOutput['5_prime_end'] = junction5Prime;
+    }
+    if (Object.keys(junction3Prime).length > 0) {
+      jsonOutput['3_prime_end'] = junction3Prime;
+    }
+
+    if (causEvent) {
+      jsonOutput.causative_event = {
+        event_type: causEvent,
+      };
+    }
+
+    setResponseJSON(JSON.stringify(jsonOutput, null, 2));
+    setResponseReadable('tbd');
+    setShowResponse(true);
   };
 
   return (
@@ -145,7 +338,7 @@ function FormParent() {
           stateFunction: handleSetProteinCoding,
         }}
       />
-      { showRfPreserved
+      {showRfPreserved
         ? (
           <FormRadio
             name="rf-preserved"
@@ -156,50 +349,82 @@ function FormParent() {
               stateFunction: handleSetRfPreserved,
             }}
           />
+
         )
         : null}
-      { showFuncDomains ? <FormFunctionalDomains /> : null }
-      { showJunctions ? <FormJunctions /> : null }
-      { showCausEvent
+      {showFuncDomains
+        ? (
+          <FormFunctionalDomains
+            setRetainedDomains={setRetainedDomains}
+            setRetainedDomainGenes={setRetainedDomainGenes}
+          />
+        )
+        : null}
+      {showJunctions
+        ? (
+          <FormJunctions
+            junction5Prime={junction5Prime}
+            setJunction5Prime={setJunction5Prime}
+            junction3Prime={junction3Prime}
+            setJunction3Prime={setJunction3Prime}
+          />
+        )
+        : null}
+      {showCausEvent
         ? (
           <FormRadio
             name="causative-event"
             prompt="Is causative event known?"
             state={{
               options: ['Yes', 'No'],
-              state: rfPreserved,
+              state: causEvent,
               stateFunction: handleSetCausEvent, // TODO
             }}
           />
         )
         : null}
-      { showCausEventInfo ? <CausEventInfo /> : null }
-      {/* { showCausEventInfo
-        ? (
-          <FormRadio
-            name="causative-event-type"
-            prompt="Select event type:"
-            state={{
-              options: ['Rearrangement', 'Read-through', 'Trans-splicing'],
-              state: rfPreserved,
-              stateFunction: handleSetRfPreserved,
-            }}
-          />
-        )
-        : null} */}
-              {/* <FormRadio name="causative-event" prompt="Is causative event known?" options={['Yes', 'No']} />
-      <FormRadio name="event-type" prompt="Select event type:" options={['Rearrangement', 'Read-through', 'Trans-splicing']} />
-      <FormRadio name="sv-info" prompt="Record structural variant information" options={['TBD']} /> */}
+      {showCausEventInfo ? <CausEventInfo /> : null}
+      {showSubmit ? <Submit handler={handleSubmit} /> : null}
+      {showResponse
+        ? <Response jsonValue={responseJSON} readableValue={responseHuman} />
+        : null}
     </>
   );
 }
 
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function Page() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="md">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Gene Fusion Curation
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box boxShadow={1}>
+          <FormParent />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Box>
+      </Container>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <div>
-    <h3>Gene Fusion Curation</h3>
-    <FormParent />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  </div>,
+  <Page />,
   document.getElementById('root'),
 );
