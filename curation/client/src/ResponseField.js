3533bd1d62dbfdaf7e55129aa1ae115845d642a9
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-07-08 13:11:23 -0400

    Add click to copy output to clipboard

--- curation/client/src/ResponseField.js
+++ curation/client/src/ResponseField.js
@@ -1,35 +1,72 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Box, TextField } from '@material-ui/core';
 
-const ResponseField = ({ jsonValue, readableValue }) => (
-  <>
-    <Box p={1}>
-      <TextField
-        id="response-json"
-        label="JSON"
-        multiline
-        variant="outlined"
-        InputProps={{
-          readOnly: true,
-        }}
-        value={jsonValue}
-        style={{ width: 700 }}
-      />
-    </Box>
-    <Box p={1}>
-      <TextField
-        id="response-hgvs"
-        label="HGVS-like"
-        multiline
-        variant="outlined"
-        InputProps={{
-          readOnly: true,
-        }}
-        value={readableValue}
-        style={{ width: 700 }}
-      />
-    </Box>
-  </>
-);
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const ResponseField = ({ jsonValue, readableValue }) => {
+  const [objectFieldLabel, setObjectFieldLabel] = useState('JSON');
+  const [readableFieldLabel, setReadableFieldLabel] = useState('HGVS-like');
+
+  async function handleObjectFieldClick(click) {
+    navigator.clipboard.writeText(jsonValue);
+    setObjectFieldLabel('copied!');
+    click.target.select();
+    await sleep(70);
+    // eslint-disable-next-line no-param-reassign
+    click.target.selectionStart = 0;
+    // eslint-disable-next-line no-param-reassign
+    click.target.selectionEnd = 0;
+    await sleep(2000);
+    setObjectFieldLabel('JSON');
+  }
+
+  async function handleReadableFieldClick(click) {
+    navigator.clipboard.writeText(readableValue);
+    setReadableFieldLabel('copied!');
+    click.target.select();
+    await sleep(70);
+    // eslint-disable-next-line no-param-reassign
+    click.target.selectionStart = 0;
+    // eslint-disable-next-line no-param-reassign
+    click.target.selectionEnd = 0;
+    await sleep(2000);
+    setReadableFieldLabel('HGVS-like');
+  }
+
+  return (
+    <>
+      <Box p={1}>
+        <TextField
+          id="response-json"
+          label={objectFieldLabel}
+          multiline
+          variant="outlined"
+          InputProps={{
+            readOnly: true,
+          }}
+          value={jsonValue}
+          onClick={(click) => handleObjectFieldClick(click)}
+          style={{ width: 700 }}
+        />
+      </Box>
+      <Box p={1}>
+        <TextField
+          id="response-hgvs"
+          label={readableFieldLabel}
+          multiline
+          variant="outlined"
+          InputProps={{
+            readOnly: true,
+          }}
+          value={readableValue}
+          onClick={(click) => handleReadableFieldClick(click)}
+          style={{ width: 700 }}
+        />
+      </Box>
+    </>
+  );
+};
 
 export default ResponseField;
