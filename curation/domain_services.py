0765ccc23b855eac8f9dd2d63168406ca9b9bb24
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-07-26 10:55:35 -0400

    Add initial skeletons

---
+++ curation/domain_services.py
@@ -0,0 +1,6 @@
+"""Provide lookup services for functional domains."""
+
+
+def get_domain_id(name):
+    """Given functional domain name, return Interpro ID."""
+    return None
--- curation/views.py
+++ curation/views.py
@@ -2,7 +2,9 @@
 from curation import app
 from flask import render_template
 from curation.gene_services import get_gene_id
+from curation.domain_services import get_domain_id
 import logging
+from typing import Dict
 
 logger = logging.getLogger('fusion_backend')
 logger.setLevel(logging.DEBUG)
@@ -15,8 +17,12 @@ def serve_static():
 
 
 @app.route('/gene/<symbol>')
-def normalize_gene(symbol):
-    """Fetch normalized concept ID given provided gene symbol."""
+def normalize_gene(symbol: str) -> Dict:
+    """Fetch normalized concept ID given provided gene symbol.
+    :param str symbol: gene symbol
+    :return: Dict (to be served as JSON) containing gene symbol, ID (as CURIE) if available, and
+        any relevant warnings
+    """
     response = {
         'symbol': symbol,
         'warnings': None
@@ -29,4 +35,26 @@ def normalize_gene(symbol):
         response['warnings'] = 'gene normalization unsuccessful'
     except Exception as e:
         logger.warning(f'Lookup of gene symbol {symbol} failed with exception {e}.')
+    return response
+
+
+@app.route('/functional-domain/<name>')
+def get_functional_domain(name: str) -> Dict:
+    """Fetch interpro ID given functional domain name.
+    :param str name: name of functional domain
+    :return: Dict (to be served as JSON) containing provided name, ID (as CURIE) if available,
+        and relevant warnings
+    """
+    response = {
+        'name': name,
+        'warnings': None
+    }
+    try:
+        domain_id = get_domain_id(name)
+        response['domain_id'] = domain_id
+    except LookupError:
+        logger.warning(f'Lookup of domain {name} failed.')
+        response['warnings'] = 'domain ID lookup unsuccessful'
+    except Exception as e:
+        logger.warning(f'Lookup of domain {name} failed with exception {e}.')
     return response
