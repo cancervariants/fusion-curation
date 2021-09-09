3b221bdefdd17d88c47ff8c69bd336ed10962087
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-09-09 16:50:34 -0400

    Remove TODO brainstorming

--- curation/validation_services.py
+++ curation/validation_services.py
@@ -8,12 +8,6 @@ def validate_fusion(fusion: Dict) -> Dict:
     """Validate fusion, returning properly-structured object along with
     any validation warnings.
 
-    TODO
-     * first, try Fusion modeling w/ validation errors if extra values ? then
-       try without + include extraneous values as warnings?
-        -- this would require a new class (eg StrictFusion) with model config value
-           `extra='forbid'`, and may not work with subclasses
-
     :param Dict fusion: incoming Fusion object
     :return: Dict containing Fusion object adhering to object model, or None +
         warnings if validation errors are raised
