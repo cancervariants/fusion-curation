c8d480a523293607ebd1761d6ed6592ea6016730
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-09-15 10:15:49 -0400

    update gene tests

--- tests/unit/test_gene.py
+++ tests/unit/test_gene.py
@@ -1,14 +1,13 @@
 """Test computation services."""
-import pytest
-from curation.gene_services import gene_service
+from curation.gene_services import get_gene_id
 
 
 def test_get_gene_id():
     """Test gene ID fetching service."""
-    gene_id, warnings = gene_service.get_gene_id('ABL1')
+    gene_id, warnings = get_gene_id('ABL1')
     assert gene_id == 'hgnc:76'
     assert warnings == []
 
-    gene_id, warnings = gene_service.get_gene_id('ABL33')
-    assert gene_id == ''
-    assert warnings == ['Lookup of gene symbol ABL33 failed.']
+    gene_id, warnings = get_gene_id('ABL33')
+    assert gene_id is None
+    assert warnings == ['Lookup of gene term ABL33 failed.']
