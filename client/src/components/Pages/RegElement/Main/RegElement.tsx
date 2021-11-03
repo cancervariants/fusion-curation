4a4852de62d23fbd4474cb15e664ddff024358cc
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-11-03 17:21:24 -0400

    Update descriptive text

--- client/src/components/Pages/RegElement/Main/RegElement.tsx
+++ client/src/components/Pages/RegElement/Main/RegElement.tsx
@@ -34,17 +34,19 @@ export const RegElement: React.FC<Props> = ({ index }) => {
       <div className='left'>
         <div className='blurb-container'>
           {
-            initialElements.current.length > 0 ?
+            regElements.length > 0 ?
               <div className='blurb'>
                 This transcript structure appears to be associated with a
+                <p />
                 {
-                  initialElements.current.map(regEl => (
-                    // eslint-disable-next-line react/jsx-key
-                    <span className='bold'>
+                  regElements.map((regEl, index) => (
+                    <span key={index} className='bold'>
                       {regEl.gene_descriptor.label.toUpperCase()} {regEl.type}
                     </span>
                   ))
-                } Regulatory Element.
+                }
+                <p />
+                Regulatory Element.
               </div>
               :
               <div className='blurb'>
