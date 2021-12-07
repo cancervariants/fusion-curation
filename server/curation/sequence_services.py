cc7f5a5ad2b41240b0b61dfaaf4110fd56317c24
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-12-07 09:53:37 -0500

    Add strand formatter

--- server/curation/sequence_services.py
+++ server/curation/sequence_services.py
@@ -3,3 +3,23 @@ import logging
 
 logger = logging.getLogger("curation_backend")
 logger.setLevel(logging.DEBUG)
+
+
+class InvalidInputException(Exception):
+    """Provide exception for input validation."""
+
+    pass
+
+
+def get_strand(input: str) -> int:
+    """Validate strand arguments received from client.
+    :param str input: strand argument from client
+    :return: correctly-formatted strand
+    :raises InvalidInputException: if strand arg is invalid
+    """
+    if input == '+':
+        return 1
+    elif input == '-':
+        return -1
+    else:
+        raise InvalidInputException
