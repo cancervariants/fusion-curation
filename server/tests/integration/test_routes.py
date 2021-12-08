0fcfff982718e99f87b73ff515a7884d486a4fdf
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-12-08 14:21:45 -0500

    Add service info tests

---
+++ server/tests/integration/test_routes.py
@@ -0,0 +1,23 @@
+"""Test end-to-end correctness of routes."""
+import re
+
+from fastapi.testclient import TestClient
+import pytest
+
+from curation.routes import app
+
+
+@pytest.fixture(scope="module")
+def client():
+    """Provide Starlette client fixture."""
+    return TestClient(app)
+
+
+def test_service_info(client: TestClient):
+    """Test /service_info endpoint"""
+    response = client.get("/service_info")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json["warnings"] is None
+    SEMVER_PATTERN = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"  # noqa: E501
+    assert re.match(SEMVER_PATTERN, response_json["version"])
