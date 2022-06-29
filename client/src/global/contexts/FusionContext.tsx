2782f8d7770bfefaadccfadc08d1e9a19fc968c3
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-06-29 11:56:24 -0400

    Temp fix for type annotation of global fusion context

--- client/src/global/contexts/FusionContext.tsx
+++ client/src/global/contexts/FusionContext.tsx
@@ -2,9 +2,7 @@
 // currently uses empty object as default but might need to make it null
 // or maybe Partial<ClientFusion> ?
 import { createContext } from 'react';
-import { ClientFusion } from '../../services/main';
 
 // the object being built by the user
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const FusionContext = createContext({} as any);
