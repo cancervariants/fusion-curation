2a0a306d0a4257df3d9d6315f058846240f9c8fc
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-07-20 19:56:02 -0400

    chore: autoformat

--- client/src/components/Pages/RegElement/RegElementForm/RegElementForm.tsx
+++ client/src/components/Pages/RegElement/RegElementForm/RegElementForm.tsx
@@ -1,19 +1,23 @@
-import { Button, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core/';
-import { makeStyles } from '@material-ui/core/styles';
-import { useContext, useState, KeyboardEvent } from 'react';
-import { v4 as uuid } from 'uuid';
-import { FusionContext } from '../../../../global/contexts/FusionContext';
-import { getGeneId } from '../../../../services/main';
 import {
-  ClientRegulatoryElement, RegulatoryElementType
-} from '../../../../services/ResponseModels';
-import { GeneAutocomplete } from '../../../main/shared/GeneAutocomplete/GeneAutocomplete';
-import './RegElementForm.scss';
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@material-ui/core/";
+import { makeStyles } from "@material-ui/core/styles";
+import { useContext, useState, KeyboardEvent } from "react";
+import { v4 as uuid } from "uuid";
+import { FusionContext } from "../../../../global/contexts/FusionContext";
+import { getGeneId } from "../../../../services/main";
+import { ClientRegulatoryElement } from "../../../../services/ResponseModels";
+import { GeneAutocomplete } from "../../../main/shared/GeneAutocomplete/GeneAutocomplete";
+import "./RegElementForm.scss";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    minWidth: '80%',
+    minWidth: "80%",
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
@@ -26,74 +30,74 @@ const RegElementForm: React.FC = () => {
   const { fusion, setFusion } = useContext(FusionContext);
   const regElements = fusion.regulatory_elements;
 
-  const [type, setType] = useState('default');
-  const [gene, setGene] = useState<string>('');
-  const [geneText, setGeneText] = useState<string>('');
+  const [type, setType] = useState("default");
+  const [gene, setGene] = useState<string>("");
+  const [geneText, setGeneText] = useState<string>("");
 
-  const inputComplete = (type !== 'default') && (gene !== '') && (geneText === '');
+  const inputComplete = type !== "default" && gene !== "" && geneText === "";
 
   const handleTypeChange = (event) => {
     setType(event.target.value);
   };
 
   const handleEnterKey = (e: KeyboardEvent) => {
-    if ((e.key == 'Enter') && inputComplete) {
+    if (e.key == "Enter" && inputComplete) {
       handleAdd();
     }
   };
 
   const handleAdd = () => {
     getGeneId(gene)
-      .then(geneResponse => {
+      .then((geneResponse) => {
         if (geneResponse.warnings?.length > 0) {
           const geneWarning = `Lookup of gene term ${gene} failed.`;
           if (geneResponse.warnings.includes(geneWarning)) {
-            setGeneText('Unrecognized term');
+            setGeneText("Unrecognized term");
           }
           throw new Error(geneWarning);
         }
 
         const cloneArray = Array.from(regElements);
         const newRegElement: ClientRegulatoryElement = {
-          type: type as RegulatoryElementType,
           element_id: uuid(),
-          gene_descriptor: {
+          type: "RegulatoryElement",
+          regulatory_class: "promoter", // TODO fix
+          associated_gene: {
             id: `gene:${geneResponse.term}`,
-            type: 'GeneDescriptor',
+            type: "GeneDescriptor",
             gene_id: geneResponse.concept_id,
             label: geneResponse.term,
-          }
+          },
         };
         cloneArray.push(newRegElement);
 
-        setFusion({ ...fusion, ...{ 'regulatory_elements': cloneArray } });
-        setGene('');
-        setType('default');
+        setFusion({ ...fusion, ...{ regulatory_elements: cloneArray } });
+        setGene("");
+        setType("default");
       })
       .catch((error) => {
         console.error(error);
-      }
-      );
+      });
   };
 
   return (
-    <div className='form-container'>
-      <div className='formInput'>
+    <div className="form-container">
+      <div className="formInput">
         <FormControl className={classes.formControl}>
-          <InputLabel id='demo-simple-select-label'>Type</InputLabel>
+          <InputLabel id="demo-simple-select-label">Type</InputLabel>
           <Select
-            labelId='demo-simple-select-label'
-            id='demo-simple-select'
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
             value={type}
             onChange={handleTypeChange}
           >
-            <MenuItem value='default' disabled></MenuItem>
-            <MenuItem value='enhancer'>Enhancer</MenuItem>
-            <MenuItem value='promoter'>Promoter</MenuItem>
+            <MenuItem value="default" disabled></MenuItem>
+            <MenuItem value="enhancer">Enhancer</MenuItem>
+            <MenuItem value="promoter">Promoter</MenuItem>
           </Select>
         </FormControl>
       </div>
-      <div className='formInput'>
+      <div className="formInput">
         <GeneAutocomplete
           selectedGene={gene}
           setSelectedGene={setGene}
@@ -102,10 +106,10 @@ const RegElementForm: React.FC = () => {
           style={{ width: 440 }}
         />
       </div>
-      <div className='regel-add-button '>
+      <div className="regel-add-button ">
         <Button
-          variant='outlined'
-          color='primary'
+          variant="outlined"
+          color="primary"
           onClick={() => handleAdd()}
           disabled={!inputComplete}
         >
