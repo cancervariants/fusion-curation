15c84f5a2561144fbab83e4799bc76b92d6efc6a
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-11-23 12:12:47 -0500

    Add page structure shared elements

---
+++ client/src/components/main/shared/TabPaper/TabPaper.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Link,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import { useColorTheme } from "../../../../global/contexts/Theme/ColorThemeContext";
+
+interface Props {
+  leftColumn: React.ReactFragment;
+  rightColumn: React.ReactFragment;
+}
+
+const TabPaper: React.FC<Props> = ({ leftColumn, rightColumn }) => {
+  const { colorTheme } = useColorTheme();
+  const useStyles = makeStyles(() => ({
+    pageContent: {
+      display: "flex",
+      backgroundColor: colorTheme["--light-gray"],
+      marginBottom: "32px",
+    },
+    leftColumn: {
+      height: "40%",
+      minHeight: "85px",
+      width: "50%",
+    },
+    leftPadder: {
+      padding: "25px",
+    },
+    dividerContainer: {
+      width: "2px",
+      display: "flex",
+      justifyContent: "center",
+    },
+    rightColumn: { width: "50%" },
+    rightPadder: { padding: "25px" },
+  }));
+  const classes = useStyles();
+  return (
+    <Paper className={classes.pageContent}>
+      <Box className={classes.leftColumn}>
+        <Box className={classes.leftPadder}>{leftColumn}</Box>
+      </Box>
+      <Box className={classes.dividerContainer}>
+        <Divider orientation="vertical" />
+      </Box>
+      <Box className={classes.rightColumn}>
+        <Box className={classes.rightPadder}>{rightColumn}</Box>
+      </Box>
+    </Paper>
+  );
+};
+
+export default TabPaper;
