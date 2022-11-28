0c76f4bb8220d83e7cf439cd66c0582fc9d05f47
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-11-28 10:30:43 -0500

    Fix tooltip styling

--- client/src/components/main/shared/HelpTooltip/HelpTooltip.tsx
+++ client/src/components/main/shared/HelpTooltip/HelpTooltip.tsx
@@ -1,27 +1,31 @@
-import { Box, makeStyles, Tooltip } from "@material-ui/core";
+import { makeStyles, Tooltip } from "@material-ui/core";
 import React from "react";
 
-const HelpTooltip = ({ title, placement, children }) => {
-  const useStyles = makeStyles(() => ({
-    tooltipContainer: {
-      // TODO this isn't working?
-      "& p": {
-        fontSize: "0.875rem",
-        lineHeight: "1.43",
-      },
+const useStylesBootstrap = makeStyles(() => ({
+  tooltip: {
+    "& p": {
+      fontSize: "0.875rem",
+      lineHeight: "1.43",
     },
-  }));
-  const classes = useStyles();
+    "& p:not(:last-child)": {
+      paddingBottom: "4px",
+    },
+  },
+}));
+
+const BootstrapTooltip = ({ title, placement, children }) => {
+  const classes = useStylesBootstrap();
 
   return (
     <Tooltip
       enterDelay={250}
       placement={placement ? placement : "right"}
       title={title}
+      classes={classes}
     >
-      <Box className={classes.tooltipContainer}>{children}</Box>
+      {children}
     </Tooltip>
   );
 };
 
-export default HelpTooltip;
+export default BootstrapTooltip;
