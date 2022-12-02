d2065444eb3e66637f4abd135309917f90ce5986
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-12-02 15:43:49 -0500

    feat: more forgiving ID parsing

--- server/curfu/routers/__init__.py
+++ server/curfu/routers/__init__.py
@@ -1,1 +1,11 @@
 """Provide specific routing methods for API endpoints."""
+
+
+def parse_identifier(identifier: str) -> str:
+    """Restructure ID value to mesh with serverside requirements
+    :param transcript: user-submitted accession identifier
+    :return: reformatted to conform to UTA requirements
+    """
+    if ":" in identifier:
+        identifier = identifier.split(":")[1]
+    return identifier
