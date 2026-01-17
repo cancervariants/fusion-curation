9d141d6a8eed5ccefd97affa99db1f6e4a68d7a8
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-01-17 14:36:53 -0500

    more

--- client/src/components/main/App/AppMenu.tsx
+++ client/src/components/main/App/AppMenu.tsx
@@ -85,7 +85,7 @@ export default function AppMenu({
       <Box className={classes.drawerContainer}>
         <Box className={classes.upperSection}>
           <Link href="/" color="inherit">
-            <h3>VICC Fusion Curation</h3>
+            <h3>Fusion-Builder</h3>
           </Link>
           <Box className={`${classes.menuHeader} ${classes.menuLink}`}>
             <Typography color="inherit">
